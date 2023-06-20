13f36796e9f610208f833b52dcc42d3eb923971b
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-20 21:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+// script.js
+
+window.onload = function() {
+  var browserInfoDiv = document.getElementById("browser-info");
+  
+  // Get browser name and version
+  var browserName = getBrowserName();
+  var browserVersion = getBrowserVersion();
+  
+  // Display browser information in the div
+  browserInfoDiv.textContent = "You are using " + browserName + " version " + browserVersion;
+}
+
+function getBrowserName() {
+  var userAgent = navigator.userAgent;
+  var browsers = ["Chrome", "Firefox", "Safari", "Opera", "Edge", "Internet Explorer"];
+  
+  for (var i = 0; i < browsers.length; i++) {
+    if (userAgent.indexOf(browsers[i]) > -1) {
+      return browsers[i];
+    }
+  }
+  
+  return "Unknown";
+}
+
+function getBrowserVersion() {
+  var userAgent = navigator.userAgent;
+  var versionIndex = userAgent.indexOf("rv:") || userAgent.indexOf("Edge/") || userAgent.indexOf("Chrome/") || userAgent.indexOf("Firefox/") || userAgent.indexOf("Version/");
+  
+  if (versionIndex > -1) {
+    return userAgent.substring(versionIndex + 3, userAgent.indexOf(" ", versionIndex));
+  }
+  
+  return "Unknown";
+}
